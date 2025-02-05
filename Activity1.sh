ff46b38a587ec24c8f78805603234fc5caf794cb
Rene Angelo Prado <152822689+RenePrado@users.noreply.github.com>
2025-02-05 01:42:16 +0000

    Hello

---
+++ Activity1.sh
@@ -0,0 +1,14 @@
+NAME=""
+AGE=0
+PROGRAM=""
+
+echo "Enter Your Name:" 
+read NAME
+
+echo "Enter Your Age:"
+read AGE
+
+echo "Enter Program:"
+read PROGRAM
+
+echo "Hello, $NAME! This is your $AGE & Your program is $PROGRAM"
---
+++ sample.sh
@@ -0,0 +1,8 @@
+USER_NAME="Rene"
+
+echo "Hello, $USER_NAME! Welcome to Bash scripting."
+
+echo "Enter your name:"
+read USER_NAME
+
+echo "Nice to meet you, $USER_NAME!"
